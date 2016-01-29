90906cabf7a5ed19cad24e5031cdeaf8ba6ceffd
lcolladotor <lcollado@jhsph.edu>
2016-01-29 14:53:43 -0500

    Update the logo links

--- header.inc.php
+++ header.inc.php
@@ -1,5 +1,5 @@
 <tr>
-	<td width="150"><div align="left"><a href="http://kabah.lcg.unam.mx/~lcollado/BPdB/index.php" target="_self"><img src="http://kabah.lcg.unam.mx/~lcollado/BPdB/<?php $logo = rand(1,4); echo "logo_BPdB_$logo.gif\"";?> width="150" height="100" border="0"/></a></div></td>
+	<td width="150"><div align="left"><a href="http://www.lcg.unam.mx/~lcollado/BPdB/index.php" target="_self"><img src="http://www.lcg.unam.mx/~lcollado/BPdB/<?php $logo = rand(1,4); echo "logo_BPdB_$logo.gif\"";?> width="150" height="100" border="0"/></a></div></td>
     <td width="150"><div align="left"><a href="http://www.unam.mx" target="_self"><img src="http://kabah.lcg.unam.mx/~lcollado/BPdB/logoUNAM.jpg" width="150" height="127" border="0"/></a></div></td>
     <td width="300"></td>
   <td width="100"><div align="center"><a href="index.php" target="_self">Home</a></div></td>
